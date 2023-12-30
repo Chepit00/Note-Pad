fc4837eed2edf281b26856457991f82f4278cfa2
Jose Silva <js38902@gmail.com>
2023-12-30 12:14:12 -0500

    added comments

--- routes/notes.js
+++ routes/notes.js
@@ -2,14 +2,16 @@ const notes = require("express").Router();
 const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
 const { v4: uuidv4 } = require("uuid");
 
+//this will get notes that are in db and parse the data 
 notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
+//this section will check if theres a title and text entered then give it an id and 
+//render a new note to db.
 notes.post("/", (req, res) => {
   const { title, text } = req.body;
 
-  if (title && text) {
     const newNote = {
       title,
       text,
@@ -17,7 +19,6 @@ notes.post("/", (req, res) => {
     };
     readAndAppend(newNote, "./db/db.json");
     res.json("Added Note!");
-  }
 });
 
 module.exports = notes;
