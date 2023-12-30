04b61e5d55332ff736f77242d06a644dcf960534
Jose Silva <js38902@gmail.com>
2023-12-30 00:03:47 -0500

    added UUID

--- routes/notes.js
+++ routes/notes.js
@@ -1,24 +1,23 @@
-const notes = require('express').Router();
-const noteList = require('../db/db.json');
+const notes = require("express").Router();
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const { v4: uuidv4 } = require("uuid");
 
-
-
-notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) =>
-        res.json(JSON.parse(data))
-    );
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-notes.post('/', (req, res) => {
-    const { title, text } = req.body;
+notes.post("/", (req, res) => {
+  const { title, text } = req.body;
 
+  if (title && text) {
     const newNote = {
-        title,
-        text,
-        
-    }
-})
-
-
+      title,
+      text,
+      id: uuidv4(),
+    };
+    readAndAppend(newNote, "./db/db.json");
+    res.json("Added Note!");
+  }
+});
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express(); 
+const api = require ('./routes/index.js')
 
-const PORT = 3001;
+const PORT = 3006;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use('/api', api);
+app.use(express.urlencoded({ extended: true })); 
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 
